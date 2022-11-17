9a5183f81cb8f21af43962365a0b52d6e16ed345
Fazman <fazmansam@gmail.com>
2022-11-17 18:01:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect,  } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
+import wordsToNumbers from "words-to-numbers";
+
+
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
+
+const alanKey ="f2a7984f708c3e0da52823e93246e4bb2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening... ");
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://i.ytimg.com/vi/rqw3OftE5sA/maxresdefault.jpg"
+          alt="Alan Logo"
+          className={classes.alanLogo}
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
+};
 
 export default App;
